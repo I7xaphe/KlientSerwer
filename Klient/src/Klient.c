2b9e62de2e1bc2161ef8bd73a0666f1503f163c2
Kamil <kamil13k@gmail.com>
2016-11-13 20:58:23 +0100

    dodonay kod do serwera -podstawowa funkcjonalosc (echo)- wyslaniew i zwrucenie argumentow przez serwer

--- Klient/src/Klient.c
+++ Klient/src/Klient.c
@@ -15,27 +15,30 @@
 
 struct argumenty {
 	char* operacja;
-	double a;
-	double b;
+	int a;
+	int b;
 	char* adres;
 };
+int dodawanie(int a, int b){
 
+	return a+b;
+}
 
 int main(int argc, char* argv[]) {
 	if (argc != 5) {
-		printf("printf podano błedna liczbe argumentów");
+		printf("podano błedna liczbe argumentów");
 		return 0;
 	}
 
-	printf("wynik liczba argumentow %d \n", argc);
-	if (atof(argv[2]) == 0) {
+	printf("argumenty :%s, %s, %s, %s. \n", argv[1],argv[2],argv[3],argv[4]);
+	if (atoi(argv[2]) == 0) {
 		if (*argv[2] != 48) {
 			printf("błedny 2 argument");
 			return -1;
 		}
 	}
 
-	if (atof(argv[3]) == 0) {
+	if (atoi(argv[3]) == 0) {
 		if (*argv[3] != 48) {
 			printf("błedny 3 argument");
 			return -1;
@@ -43,12 +46,12 @@ int main(int argc, char* argv[]) {
 	}
 	struct argumenty userArgs;
 	userArgs.operacja = argv[1];
-	userArgs.a = atof(argv[2]);
-	userArgs.b = atof(argv[3]);
-	userArgs.adres = atoi(argv[4]);
+	userArgs.a = atoi(argv[2]);
+	userArgs.b = atoi(argv[3]);
+	userArgs.adres = argv[4];
 
 	if (!strcmp(userArgs.operacja, "dodawanie")) {
-
+		printf("dodawanie lokalne %d \n",dodawanie(userArgs.a,userArgs.b));
 	} else if (!strcmp(userArgs.operacja, "odejmowanie")) {
 
 	} else if (!strcmp(userArgs.operacja, "mnozenie")) {
@@ -61,20 +64,20 @@ int main(int argc, char* argv[]) {
 	}
 
 	int clientSocket, portNum, nBytes;
-	char buffer[1024];
+	char buffer[1024]="";
 	struct sockaddr_in serverAddr;
 	socklen_t addr_size;
 	clientSocket = socket(PF_INET, SOCK_DGRAM, 0);
 
 	/*Configure settings in address struct*/
 	serverAddr.sin_family = AF_INET;
-	serverAddr.sin_port = htons(7891);
+	serverAddr.sin_port = htons(8888);
 	serverAddr.sin_addr.s_addr = inet_addr(userArgs.adres);
 	memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);
 
 	addr_size = sizeof serverAddr;
 
-	nBytes=sprintf (buffer,"%.3f %.3f %s", userArgs.a,userArgs.b, userArgs.operacja);
+	nBytes=sprintf(buffer,"%d %d %s", userArgs.a,userArgs.b, userArgs.operacja);
 	nBytes++;
 
 	/*Send message to server*/
@@ -84,7 +87,7 @@ int main(int argc, char* argv[]) {
 	/*Receive message from server*/
 	nBytes = recvfrom(clientSocket, buffer, 1024, 0, NULL, NULL);
 
-	printf("Received from server: %s\n", buffer);
+	printf("Zwrocony wynik  : %s\n", buffer);
 
 	return 0;
 }
